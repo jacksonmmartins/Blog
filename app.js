6250af2bdbbd1f70960fa053ddf5a12c41459ad8
Jackson <jacksondesobral@hotmail.com>
2022-08-03 19:25:50 -0300

    Blog_sem_banco_e_card_formulário

---
+++ app.js
@@ -0,0 +1,38 @@
+// configurações do express
+// carregando modulos
+const express = require('express');
+const handlebars = require('express-handlebars');
+const app = express();
+const bodyParser = require('body-parser');
+//pegando rotas adimin
+const admin = require('./routs/admin');
+//trabalhando com diretórios
+const path = require('path')
+//const mongoose = require('mongoose');
+
+// configurações
+    //body parser
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(bodyParser.json())
+    //handlebars
+    const hbs = handlebars.create({
+        defaultLayout: "main"
+    })
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+    //mongoose
+
+
+//rotas
+    app.get('/', (req,res)=>{
+        res.send('Está é a rota principal')
+    })
+    app.use('/admin', admin)
+//public referencia dos arquivos estáticos, caminho absoluto
+    app.use(express.static(path.join(__dirname,'public')))
+
+//outros
+const port = 3000;
+app.listen(port, ()=> {
+    console.log(`A aplicação está rodando no endereço http://localhost:${port}`);
+})
---
+++ routs/admin.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req,res)=>{
+    res.render("admin/index")
+})
+
+router.get('/post', (req,res)=>{
+    res.send('Página de posts')
+})
+
+router.get('/categorias', (req,res) =>{
+    res.render("./admin/categorias")
+})
+
+router.get('/categorias/add', (req,res)=>{
+    res.render("admin/addcategorias")
+})
+
+module.exports = router
